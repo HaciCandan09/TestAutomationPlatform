e07949ee16f7637e195a58d5cd704c50d2402311
HaciCandan09 <116837074+HaciCandan09@users.noreply.github.com>
2026-05-19 11:32:49 +0200

    Extend test step definition

--- TestAutomationPlatform/DTO/Teststep.cs
+++ TestAutomationPlatform/DTO/Teststep.cs
@@ -1,9 +1,10 @@
-﻿namespace TestAutomationPlatform.DTO
+namespace TestAutomationPlatform.DTO
 {
     public class TestStep
     {
         public string? Action { get; set; }
         public string? Selector { get; set; }
         public string? Value { get; set; }
+        public int? Timeout { get; set; }
     }
 }
