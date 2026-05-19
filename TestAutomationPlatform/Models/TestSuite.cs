15451b0676bc8541ce3b34f816baf2f42a6a43b9
HaciCandan09 <116837074+HaciCandan09@users.noreply.github.com>
2026-05-19 12:03:09 +0200

    Harden test suite model defaults

--- TestAutomationPlatform/Models/TestSuite.cs
+++ TestAutomationPlatform/Models/TestSuite.cs
@@ -1,10 +1,10 @@
-﻿using System.Text.Json.Serialization;
+using System.Text.Json.Serialization;
 using TestAutomationPlatform.Models;
 
 public class TestSuite
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 
     public int WorkspaceId { get; set; }
 
