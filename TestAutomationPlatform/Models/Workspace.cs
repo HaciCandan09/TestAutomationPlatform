a9a86e52f4ec33edba468a581f23ab02432be113
HaciCandan09 <116837074+HaciCandan09@users.noreply.github.com>
2026-05-19 12:01:55 +0200

    Harden workspace model defaults

--- TestAutomationPlatform/Models/Workspace.cs
+++ TestAutomationPlatform/Models/Workspace.cs
@@ -1,10 +1,9 @@
-﻿using System.Text.Json.Serialization;
 using TestAutomationPlatform.Models;
 
 public class Workspace
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 
     public List<Script> Scripts { get; set; } = new();
     public List<TestSuite> TestSuites { get; set; } = new();
