d36fb3834c18c7af0431b95ecf356b2df4b52860
HaciCandan09 <116837074+HaciCandan09@users.noreply.github.com>
2026-05-19 12:36:07 +0200

    Harden category model defaults

--- TestAutomationPlatform/Models/Category.cs
+++ TestAutomationPlatform/Models/Category.cs
@@ -1,11 +1,9 @@
-﻿using System.Text.Json.Serialization;
 using TestAutomationPlatform.Models;
 
 public class Category
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 
-   
     public List<Script> Scripts { get; set; } = new();
 }
